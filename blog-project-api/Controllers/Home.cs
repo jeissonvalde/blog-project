929741e92bf6105532b376304da7646d896af831
Jeisson A Valderrama <jeissonvalderramam@gmail.com>
2023-01-02 10:52:14 -0500

    Agregar archivos de proyecto.

---
+++ blog-project-api/Controllers/Home.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using blog_project_api.Models;
+
+namespace blog_project_api.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        [Route("articulo")]
+        public dynamic getArticleById()
+        {
+            return new
+            {
+                title = "Messi"
+            };
+        }
+
+        [HttpGet]
+        [Route("articles")]
+        public dynamic getArticles()
+        {
+            List<Article> articles = new List<Article>
+        {
+            new Article
+            {
+                id = "as09aj332",
+                content = "Esta es el artículo.",
+                createdAt = new DateTime(),
+                bgImage = "some-url"
+            }
+        };
+
+            return articles;
+        }
+    }
+}
---
+++ blog-project-api/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace blog_project_api.Models
+{
+    public class Article
+    {
+        public string id { get; set; }
+        public string content { get; set; }
+        public DateTime createdAt { get; set; }
+        public object bgImage { get; set; }
+    }
+}
